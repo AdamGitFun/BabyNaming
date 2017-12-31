0babc7496d93cccba8c59d4ed38c692be7a85e13
AGit <30912338+AdamGitFun@users.noreply.github.com>
2017-12-30 22:57:54 -0500

    adjusts x location for generate scroll

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -45,10 +45,11 @@ function generateNameCombinations() {
 function scrollToResult() {
 
   var button_bounding_rectangle = document.getElementById("generateButton").getBoundingClientRect();
-  var button_position = button_bounding_rectangle.bottom;
+  var button_y_position = button_bounding_rectangle.bottom;
+  var button_x_position = button_bounding_rectangle.right-button_bounding_rectangle.left;
   console.log(button_bounding_rectangle.bottom);
 
-  window.scrollTo(0,button_position);
+  window.scrollTo(button_x_position,button__y_position);
 }
 
 function aggregateNames(rawfirstNames, rawmiddleNames, rawlastNames) {
